96054a0a9b9d6848cf17a2dd6f7598c3b77fbfdb
cskitsune <anaclara.zoppiserpa@proton.me>
2025-11-30 18:53:47 -0300

    seed inspection for unit test design

--- projects/rinha-de-neopets/src/main.rs
+++ projects/rinha-de-neopets/src/main.rs
@@ -1,10 +1,13 @@
 mod neopets;
 mod battle;
+mod utils;
 
 use neopets::load_neopets;
 use battle::battle_loop;
+use utils::inspect_seed;
 
 fn main() {
-    let neopets_set = load_neopets("assets/neopets.json");
-    battle_loop(&neopets_set[0], &neopets_set[1]);
+    //let neopets_set = load_neopets("assets/neopets.json");
+    //battle_loop(&neopets_set[0], &neopets_set[1]);
+    inspect_seed(42, 10);
 }
---
+++ projects/rinha-de-neopets/src/utils.rs
@@ -0,0 +1,25 @@
+use rand::Rng;
+
+pub fn inspect_seed(seed: u64, count: usize) {
+    use rand::SeedableRng;
+    use rand::rngs::StdRng;
+
+    let mut rng = StdRng::seed_from_u64(seed);
+    println!("Seed {} produces:", seed);
+
+    for i in 0..count {
+        let val: f64 = rng.random();
+        println!("  [{}] = {:.6}", i, val);
+    }
+    // Outputs
+    //   [0] = 0.526557
+  // [1] = 0.542725
+  // [2] = 0.636465
+  // [3] = 0.405902
+  // [4] = 0.034343
+  // [5] = 0.414957
+  // [6] = 0.737424
+  // [7] = 0.849252
+  // [8] = 0.131279
+  // [9] = 0.003252
+}
