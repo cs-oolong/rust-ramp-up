b8947f6692ef52cf9f393af0fee14a143fdcfdc6
cskitsune <anaclara.zoppiserpa@proton.me>
2025-11-30 18:43:29 -0300

    battle loop wip, dependency injection for choose_action

--- projects/rinha-de-neopets/src/battle.rs
+++ projects/rinha-de-neopets/src/battle.rs
@@ -5,16 +5,17 @@ fn roll_d20() -> u8 {
     rand::rng().random_range(1..=20)
 }
 
+#[derive(Debug)]
 enum Action {
     Attack,
     CastSpell(usize),
     Heal,
 }
 
-fn choose_action(neopet: &Neopet) -> Action {
-    let roll : f64 = rand::rng().random();
+fn choose_action<R: Rng>(neopet: &Neopet, rng: &mut R) -> Action {
+    let roll : f64 = rng.random();
     if roll < neopet.behavior.attack_chance {
-        Action::Attack;
+        Action::Attack
     } else if roll < neopet.behavior.attack_chance + neopet.behavior.heal_chance {
         Action::Heal
     } else {
@@ -55,9 +56,16 @@ pub fn battle_loop(fighter1: &Neopet, fighter2: &Neopet) {
     let mut battle_in_progress = true;
     let max_turns = 10;
 
-    let mut turn = 1;
-    while battle_in_progress && turn <= max_turns {
-
+    let mut turn = 0;
+    while battle_in_progress && turn < max_turns {
+        let first_action = choose_action(first, &mut rand::rng());
+        println!("{}: {:?}", first.name, first_action);
+        turn += 1;
+        println!("turn {turn}");
+        let second_action = choose_action(second, &mut rand::rng());
+        println!("{}: {:?}", second.name, second_action);
+        turn += 2;
+        println!("turn {turn}");
     }
     // Turns start, respecting the initiative result.
 }
