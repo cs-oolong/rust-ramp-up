a3bb8a63058f81d35e6e9b5d8f5d342103d4d180
cskitsune <anaclara.zoppiserpa@proton.me>
2025-12-02 17:16:14 -0300

    removing seed helpers

--- projects/rinha-de-neopets/src/main.rs
+++ projects/rinha-de-neopets/src/main.rs
@@ -1,13 +1,10 @@
 mod battle;
 mod neopets;
-mod utils;
 
 use battle::battle_loop;
 use neopets::load_neopets;
-use crate::utils::inspect_seed_for_d20;
 
 fn main() {
-    //let neopets_set = load_neopets("assets/neopets.json");
-    //battle_loop(&neopets_set[0], &neopets_set[1], &mut rand::rng());
-    inspect_seed_for_d20(25, 10);
+    let neopets_set = load_neopets("assets/neopets.json");
+    battle_loop(&neopets_set[0], &neopets_set[1], &mut rand::rng());
 }
